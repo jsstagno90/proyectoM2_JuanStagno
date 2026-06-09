12e3f702602ae7959a1d0fb67cbb131598136e8f
Juan Stagno <jsstagno@hotmail.com>
2026-06-09 08:59:11 -0300

    CRUD terminado, de posts y authors

--- src/controllers/authors.controller.js
+++ src/controllers/authors.controller.js
@@ -1,5 +1,9 @@
 const { getAllAuthors,
         getAuthorById,
+        createAuthor,
+        updateAuthor,
+        deleteAuthor
+
 } = require("../services/authors.service");
 
 console.log("SERVICE:", {
@@ -40,7 +44,103 @@ async function getAuthor(req, res) {
     }
 }
 
+async function createNewAuthor(req, res) {
+    try {
+        const { name, email, bio } = req.body;
+
+        if (!name || !email) {
+            return res.status(400).json({
+                message: "name y email son obligatorios"
+            });
+        }
+
+        const newAuthor = await createAuthor(
+            name,
+            email,
+            bio
+        );
+
+        res.status(201).json(newAuthor);
+
+    } catch (error) {
+
+        if (error.code === "23505") {
+            return res.status(400).json({
+                message: "El email ya existe"
+            });
+        }
+
+        res.status(500).json({
+            message: "Error al crear autor",
+            error: error.message
+        });
+    }
+}
+
+async function updateAuthorById(req, res) {
+    try {
+        const { name, email, bio } = req.body;
+
+        if (!name || !email) {
+            return res.status(400).json({
+                message: "name y email son obligatorios"
+            });
+        }
+
+        const updatedAuthor = await updateAuthor(
+            req.params.id,
+            name,
+            email,
+            bio
+        );
+
+        if (!updatedAuthor) {
+            return res.status(404).json({
+                message: "Autor no encontrado"
+            });
+        }
+
+        res.json(updatedAuthor);
+
+    } catch (error) {
+
+        if (error.code === "23505") {
+            return res.status(400).json({
+                message: "El email ya existe"
+            });
+        }
+
+        res.status(500).json({
+            message: "Error al actualizar autor",
+            error: error.message
+        });
+    }
+}
+
+async function deleteAuthorById(req, res) {
+    try {
+        const deletedAuthor = await deleteAuthor(req.params.id);
+
+        if (!deletedAuthor) {
+            return res.status(404).json({
+                message: "Autor no encontrado"
+            });
+        }
+
+        res.status(204).send();
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Error al eliminar autor",
+            error: error.message
+        });
+    }
+}
+
 module.exports = {
     getAuthors,
-    getAuthor
+    getAuthor,
+    createNewAuthor,
+    updateAuthorById,
+    deleteAuthorById
 };
--- src/routes/authors.routes.js
+++ src/routes/authors.routes.js
@@ -2,10 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 const { getAuthors,
-    getAuthor
+    getAuthor,
+    createNewAuthor,
+    updateAuthorById,
+    deleteAuthorById
  } = require("../controllers/authors.controller");
 
 router.get("/", getAuthors);
 router.get("/:id", getAuthor);
+
+router.post("/", createNewAuthor);
+
+router.put("/:id", updateAuthorById);
+
+router.delete("/:id", deleteAuthorById);
 
 module.exports = router;
