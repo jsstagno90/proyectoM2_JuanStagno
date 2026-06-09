023bc69f6e111c3201d4999f4d12abafe17316b7
Juan Stagno <jsstagno@hotmail.com>
2026-06-09 11:09:56 -0300

    Probando database.js para railway

--- src/db/database.js
+++ src/db/database.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
+    user: process.env.DB_USER || process.env.PGUSER,
+    password: process.env.DB_PASSWORD || process.env.PGPASSWORD,
+    host: process.env.DB_HOST || process.env.PGHOST,
+    port: process.env.DB_PORT || process.env.PGPORT,
+    database: process.env.DB_NAME || process.env.PGDATABASE,
 });
 
 module.exports = pool;
